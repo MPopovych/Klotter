858ccbd811b2b315b6c04fb1c2618ecdcf1be9ca
maxpopovych <vwotar@gmail.com>
2023-08-09 18:42:01 +0200

    icon and gradle task

--- src/main/kotlin/com/makki/klotter/utils/MainUtil.kt
+++ src/main/kotlin/com/makki/klotter/utils/MainUtil.kt
@@ -1,6 +1,9 @@
 package com.makki.klotter.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.FrameWindowScope
@@ -12,6 +15,7 @@ import kotlin.system.exitProcess
 fun SimpleWindow(
 	size: DpSize?,
 	name: String = "Untitled",
+	icon: ImageVector? = null,
 	exit: (() -> Unit)? = null,
 	content: @Composable FrameWindowScope.() -> Unit,
 ) {
@@ -20,6 +24,7 @@ fun SimpleWindow(
 			exit?.invoke() ?: exitProcess(0)
 		},
 		title = name,
+		icon = icon?.let { rememberVectorPainter(icon) },
 		state = WindowState(size = size ?: DpSize(600.dp, 300.dp)),
 	) {
 		content()
